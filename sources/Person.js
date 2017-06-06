8acaa9f9e1410b89f663241cc7b5e31e1ce4cb84
kkrsona1234 <kausalyarani.krishnasamy9@t-mobile.com>
2017-06-06 00:53:33 -0700

    Update Person.js

--- sources/Person.js
+++ sources/Person.js
@@ -1,10 +1,14 @@
-
 var Person = function(first, last, middle) {
     this.first = first;
     this.middle = middle;
     this.last = last;
 };
-
+var address = function (street, city, state){ 
+    
+    this.street = street;
+    this.city = city;
+    this.state = state;
+};
 
 Person.prototype = {
 
@@ -13,5 +17,9 @@ Person.prototype = {
     }
 
     
-    
+    address.prototype = {
+
+    myaddress : function() {
+        return this.street + (this.city ? ' ' + this.city: '') + ' ' + this.state;
+    }
 };
