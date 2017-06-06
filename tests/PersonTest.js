83c8c0621848cd09c01367d5a408e0ce5f481473
System Administrator <root@WABELMAC0698546.local>
2017-06-06 13:55:42 -0700

    aa

--- tests/PersonTest.js
+++ tests/PersonTest.js
@@ -8,7 +8,7 @@ TestCase('PersonTest', {
         var p = new Person('John', 'Doe', 'P.');
         assertEquals('Should have responded with full name', 'John P. Doe', p.whoAreYou());
     },
-testWhoAreYou : function() {}
-    
+// testWhoAreYou : function() {}
+   testaddress : function () {} 
 
 });
