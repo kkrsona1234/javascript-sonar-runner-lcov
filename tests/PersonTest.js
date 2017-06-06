c9bbbe3ef4fbaac1b9ebed0115d37c554ef74409
kkrsona1234 <kausalyarani.krishnasamy9@t-mobile.com>
2017-06-06 00:54:08 -0700

    Update PersonTest.js

--- tests/PersonTest.js
+++ tests/PersonTest.js
@@ -4,6 +4,10 @@ TestCase('PersonTest', {
         var p = new Person('John', 'Doe', 'P.');
         assertEquals('Should have responded with full name', 'John P. Doe', p.whoAreYou());
     },
+    testWhoAreYou : function() {
+        var p = new Person('John', 'Doe', 'P.');
+        assertEquals('Should have responded with full name', 'John P. Doe', p.whoAreYou());
+    },
 testWhoAreYou : function() {}
     
 
